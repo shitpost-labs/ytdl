2f8227dc74397da4fb9d2fb165f880d9096c593b
dev-bun <66565286+dev-bun@users.noreply.github.com>
2024-01-07 18:31:42 +0000

    my god im braindead

--- next.config.js
+++ next.config.js
@@ -6,36 +6,6 @@ const nextConfig = {
   },
   rewrites: async () => [
     {
-      source: '/:path',
-      has: [
-        {
-          type: 'query',
-          key: 'v',
-        }
-      ],
-      destination: '/api/vid?id=:v',
-    },
-    {
-      source: '/:path',
-      has: [
-        {
-          type: 'query',
-          key: 'id',
-        }
-      ],
-      destination: '/api/vid?id=:id',
-    },
-    {
-      source: '/>path',
-      has: [
-        {
-          type: 'query',
-          key: 'url',
-        }
-      ],
-      destination: '/api/dl?url=:url',
-    },
-    {
       source: '/:id',
       destination: '/api/vid?id=:id',
     }
